fixed bug that stopped gradients from flowing in scalar case

examples/linear_regression.py
@@ -7,6 +7,8 @@ import jax
 import jax.numpy as jnp
 import jax.lax as lax
 import jax.random as jrandom
+from jax.test_util import check_grads
+
 import numpy as np
 import optax
 import distrax
@@ -191,14 +193,14 @@ def prepare_data(batch: Batch, prng_key: Optional[PRNGKey] = None) -> Array:
 # ----------------------------
 @hk.without_apply_rng
 @hk.transform
-def log_prob(data: Array, theta: Array, d: Array, xi: Array) -> Array:
+def log_prob(data: Array, theta: Array, xi: Array) -> Array:
     # Get batch
     shift = data.mean(axis=0)
     scale = data.std(axis=0) + 1e-14
 
     model = make_nsf(
         event_shape=EVENT_SHAPE,
-        cond_info_shape=cond_info_shape,
+        # cond_info_shape=cond_info_shape,
         num_layers=flow_num_layers,
         hidden_sizes=[hidden_size] * mlp_num_layers,
         num_bins=num_bins,
@@ -209,15 +211,15 @@ def log_prob(data: Array, theta: Array, d: Array, xi: Array) -> Array:
         shift=shift,
         scale=scale,
     )
-    return model.log_prob(data, theta, d, xi)
+    return model.log_prob(data, theta, xi)
 
 
 @hk.without_apply_rng
 @hk.transform
-def model_sample(key: PRNGKey, num_samples: int, theta: Array, d: Array, xi: Array) -> Array:
+def model_sample(key: PRNGKey, num_samples: int, theta: Array, xi: Array) -> Array:
     model = make_nsf(
         event_shape=EVENT_SHAPE,
-        cond_info_shape=cond_info_shape,
+        # cond_info_shape=cond_info_shape,
         num_layers=flow_num_layers,
         hidden_sizes=[hidden_size] * mlp_num_layers,
         num_bins=num_bins,
@@ -226,7 +228,7 @@ def model_sample(key: PRNGKey, num_samples: int, theta: Array, d: Array, xi: Arr
         use_resnet=True,
         event_dim=EVENT_DIM,
     )
-    return model._sample_n(key=key, n=[num_samples], theta=theta, d=d, xi=xi)
+    return model._sample_n(key=key, n=[num_samples], theta=theta, xi=xi)
 
 
 def loss_fn(
@@ -237,20 +239,21 @@ def loss_fn(
 
 
 def unified_loss_fn(
-    params: hk.Params, prng_key: PRNGKey, x: Array, theta: Array, d: Array
+    params: hk.Params, prng_key: PRNGKey, x: Array, theta: Array
 ) -> Array:
-    xi = params['xi']
+    xi = jnp.asarray(params['xi'])
     xi = jnp.broadcast_to(xi, (len(x), len(xi)))
     flow_params = {k: v for k, v in params.items() if k != 'xi'}
+    # breakpoint()
     # Loss is average negative log likelihood.
-    loss = -jnp.mean(log_prob.apply(flow_params, x, theta, d, xi))
+    loss = -jnp.mean(log_prob.apply(flow_params, x, theta, xi))
     return loss
 
 
 @jax.jit
 def eval_fn(params: hk.Params, batch: Batch) -> Array:
     x, theta, d, xi = prepare_data(batch)
-    loss = -jnp.mean(log_prob.apply(params, x, theta, d, xi))
+    loss = -jnp.mean(log_prob.apply(params, x, theta, xi))
     return loss
 
 
@@ -263,7 +266,7 @@ def update(
     x, theta, d, xi = prepare_data(batch)
     # grads = jax.grad(loss_fn)(params, prng_key, x, theta, d, xi)
     # grads_d = jax.grad(loss_fn, argnums=5)(params, prng_key, x, theta, d, xi)
-    grads = jax.grad(unified_loss_fn)(params, prng_key, x, theta, d)
+    grads = jax.grad(unified_loss_fn)(params, prng_key, x, theta)
     updates, new_opt_state = optimizer.update(grads, opt_state)
     new_params = optax.apply_updates(params, updates)
     return new_params, new_opt_state#, grads_d
@@ -344,7 +347,7 @@ if __name__ == "__main__":
     EVENT_SHAPE = (len(d_sim),)
     # EVENT_DIM is important for the normalizing flow's block.
     EVENT_DIM = 1
-    cond_info_shape = (theta_shape[0], len_d, len_xi)
+    # cond_info_shape = (theta_shape[0], len_d, len_xi)
 
     num_samples = 2
     inner_samples = 10 # AKA M or L in BOED parlance
@@ -367,7 +370,7 @@ if __name__ == "__main__":
         next(prng_seq),
         np.zeros((1, *EVENT_SHAPE)),
         np.zeros((1, *theta_shape)),
-        np.zeros((1, *d_shape)),
+        # np.zeros((1, *d_shape)),
         np.zeros((1, *xi_shape)),
     )
     params['xi'] = xi
@@ -403,6 +406,8 @@ if __name__ == "__main__":
             params, next(prng_seq), opt_state, next(train_ds)
         )
 
+        # x, theta, d, xi = prepare_data(next(train_ds))
+        # print(check_grads(unified_loss_fn, (params, next(prng_seq), jnp.asarray(x), jnp.asarray(theta)), order=2))
         print(f"STEP: {step:5d}; Xi: {params['xi']}")
         if step % eval_frequency == 0:
             val_loss = eval_fn(params, next(valid_ds))

src/lfiax/bijectors/masked_coupling_conditional.py
@@ -38,7 +38,7 @@ class MaskedConditionalCoupling(MaskedCoupling):
         super().__init__(**kwargs)
 
     def forward_and_log_det(
-        self, x: Array, theta: Array, d: Array, xi: Array
+        self, x: Array, theta: Array, xi: Array
     ) -> Tuple[Array, Array]:
         """Computes y = f(x|z) and log|det J(f)(x|z)|."""
         self._check_forward_input_shape(x)
@@ -54,11 +54,23 @@ class MaskedConditionalCoupling(MaskedCoupling):
         # params = jax.lax.cond(pred, 
         #                 lambda: self._conditioner(theta, xi),
         #                 lambda: self._conditioner(masked_x, theta, d, xi))
-        params = self._conditioner(masked_x, theta, d, xi)
+        params = self._conditioner(masked_x, theta, xi)
         # breakpoint()
         # jax.debug.print("params: {}", params)
         y0, log_d = self._inner_bijector(params).forward_and_log_det(x)
-        y = jnp.where(self._event_mask, x, y0)
+        # def loss_fun(params):
+        #     y, logdet = self._inner_bijector(params).forward_and_log_det(x)
+        #     return -logdet
+        
+        # grads = jax.grad(loss_fun)(params)
+        # jax.debug.print("Gradients of the loss function with respect to the conditioning network's parameters: {}", grads)
+        # jax.debug.print("unmasked y: {}", y0)
+        # TODO: Check that this makes sense...
+        if masked_x.shape[1] > 1:
+            y = jnp.where(self._event_mask, x, y0)
+        else:
+            y = y0
+        # jax.debug.print("masked y: {}", y)
         logdet = math.sum_last(
             jnp.where(self._mask, 0.0, log_d),
             self._event_ndims - self._inner_event_ndims,
@@ -66,7 +78,7 @@ class MaskedConditionalCoupling(MaskedCoupling):
         return y, logdet
 
     def inverse_and_log_det(
-        self, y: Array, theta: Array, d: Array, xi: Array
+        self, y: Array, theta: Array, xi: Array
     ) -> Tuple[Array, Array]:
         """Computes x = f^{-1}(y|z) and log|det J(f^{-1})(y|z)|."""
         self._check_inverse_input_shape(y)
@@ -80,7 +92,7 @@ class MaskedConditionalCoupling(MaskedCoupling):
         # params = jax.lax.cond(pred, 
         #                 lambda: self._conditioner(theta, xi),
         #                 lambda: self._conditioner(masked_y, theta, d, xi))
-        params = self._conditioner(masked_y, theta, d, xi)
+        params = self._conditioner(masked_y, theta, xi)
         x0, log_d = self._inner_bijector(params).inverse_and_log_det(y)
         x = jnp.where(self._event_mask, y, x0)
         logdet = math.sum_last(

src/lfiax/bijectors/standardizing_conditional.py
@@ -23,7 +23,7 @@ class StandardizingBijector(ScalarAffine):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def forward(self, x: Array, theta: Array, d: Array, xi: Array) -> Array:
+    def forward(self, x: Array, theta: Array, xi: Array) -> Array:
         """Computes y = f(x) = \mu + x + \sigma. Ignores z."""
         batch_shape = jax.lax.broadcast_shapes(self._batch_shape, x.shape)
         batched_scale = jnp.broadcast_to(self._scale, batch_shape)
@@ -31,21 +31,21 @@ class StandardizingBijector(ScalarAffine):
         return batched_scale * x + batched_shift
 
     def forward_log_det_jacobian(
-        self, x: Array, theta: Array, d: Array, xi: Array
+        self, x: Array, theta: Array, xi: Array
     ) -> Array:
         """Computes log|det J(f)(x)| ignoring z."""
         batch_shape = jax.lax.broadcast_shapes(self._batch_shape, x.shape)
         return jnp.broadcast_to(self._log_scale, batch_shape)
 
     def forward_and_log_det(
-        self, x: Array, theta: Array, d: Array, xi: Array
+        self, x: Array, theta: Array, xi: Array
     ) -> Tuple[Array, Array]:
         """Computes y = f(x) and log|det J(f)(x)| ignoring z."""
-        return self.forward(x, theta, d, xi), self.forward_log_det_jacobian(
-            x, theta, d, xi
+        return self.forward(x, theta, xi), self.forward_log_det_jacobian(
+            x, theta, xi
         )
 
-    def inverse(self, y: Array, theta: Array, d: Array, xi: Array) -> Array:
+    def inverse(self, y: Array, theta: Array, xi: Array) -> Array:
         """Computes x = f^{-1}(y) ignoring z."""
         batch_shape = jax.lax.broadcast_shapes(self._batch_shape, y.shape)
         batched_inv_scale = jnp.broadcast_to(self._inv_scale, batch_shape)
@@ -53,16 +53,16 @@ class StandardizingBijector(ScalarAffine):
         return batched_inv_scale * (y - batched_shift)
 
     def inverse_log_det_jacobian(
-        self, y: Array, theta: Array, d: Array, xi: Array
+        self, y: Array, theta: Array, xi: Array
     ) -> Array:
         """Computes log|det J(f^{-1})(y)| ignoring z."""
         batch_shape = jax.lax.broadcast_shapes(self._batch_shape, y.shape)
         return jnp.broadcast_to(jnp.negative(self._log_scale), batch_shape)
 
     def inverse_and_log_det(
-        self, y: Array, theta: Array, d: Array, xi: Array
+        self, y: Array, theta: Array, xi: Array
     ) -> Tuple[Array, Array]:
         """Computes x = f^{-1}(y) and log|det J(f^{-1})(y)| ignoring z."""
-        return self.inverse(y, theta, d, xi), self.inverse_log_det_jacobian(
-            y, theta, d, xi
+        return self.inverse(y, theta, xi), self.inverse_log_det_jacobian(
+            y, theta, xi
         )

src/lfiax/distributions/transformed_conditional.py
@@ -22,23 +22,23 @@ class ConditionalTransformed(Transformed):
         super().__init__(distribution, flow)
 
     def _sample_n(
-        self, key: PRNGKey, n: int, theta: Array, d: Array, xi: Array
+        self, key: PRNGKey, n: int, theta: Array, xi: Array
     ) -> Array:
         """Returns `n` samples conditioned on `z`."""
         x = self.distribution.sample(seed=key, sample_shape=n)
         # breakpoint()
-        y, _ = self.bijector.forward_and_log_det(x, theta, d, xi)
+        y, _ = self.bijector.forward_and_log_det(x, theta, xi)
         return y
 
-    def log_prob(self, value: Array, theta: Array, d: Array, xi: Array) -> Array:
+    def log_prob(self, value: Array, theta: Array, xi: Array) -> Array:
         """See `Distribution.log_prob`."""
-        x, ildj_y = self.bijector.inverse_and_log_det(value, theta, d, xi)
+        x, ildj_y = self.bijector.inverse_and_log_det(value, theta, xi)
         lp_x = self.distribution.log_prob(x)
         lp_y = lp_x + ildj_y
         return lp_y
 
     def _sample_n_and_log_prob(
-        self, key: PRNGKey, n: int, theta: Array, d: Array, xi: Array
+        self, key: PRNGKey, n: int, theta: Array, xi: Array
     ) -> Tuple[Array, Array]:
         """Returns `n` samples and their log probs depending on `z`.
 
@@ -54,6 +54,6 @@ class ConditionalTransformed(Transformed):
           A tuple of `n` samples and their log probs.
         """
         x, lp_x = self.distribution.sample_and_log_prob(seed=key, sample_shape=n)
-        y, fldj = jax.vmap(self.bijector.forward_and_log_det)(x, theta, d, xi)
+        y, fldj = jax.vmap(self.bijector.forward_and_log_det)(x, theta, xi)
         lp_y = jax.vmap(jnp.subtract)(lp_x, fldj)
         return y, lp_y

src/lfiax/nets/scalar_conditioners.py
@@ -52,7 +52,6 @@ class ScalarConditionerModule(hk.Module):
 
 def scalar_conditioner_mlp(
     event_shape: Sequence[int],
-    cond_info_shape: Sequence[int],
     hidden_sizes: Sequence[int],
     num_bijector_params: int,
     standardize_theta: bool = False,
@@ -76,14 +75,19 @@ def scalar_conditioner_mlp(
                     z += hk.Linear(hidden)(hk.Flatten()(z))
             else:
                 z = hk.nets.MLP(hidden_sizes, activate_final=True)(z)
+            # jax.debug.print("z prev: {}", z)
+            # breakpoint()
             z = hk.Linear(
                 np.prod(event_shape) * num_bijector_params,
                 w_init=jnp.zeros,
                 b_init=jnp.zeros,
             )(z)
+            # hk.Linear(np.prod(event_shape) * num_bijector_params, w_init=jnp.zeros, b_init=jnp.zeros)(z)
+            # jax.debug.print("z: {}", z)
             z = hk.Reshape(
                 tuple(event_shape) + (num_bijector_params,), preserve_dims=-1
             )(z)
+            # jax.debug.print("z: {}", z)
             return z
 
     return ScalarConditionerModule()